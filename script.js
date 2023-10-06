0da0f0f15a9fdd85fa29a07ffa11506947722abe
rajatya713 <rajatya713@gmail.com>
2023-10-06 19:01:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let speechRecognition = window.webkitSpeechRecognition
+let recognition = new speechRecognition()
+let textbox = $("#text_box")
+let content = ""
+recognition.continuous = true
+let counter = $("#counter")
+
+
+recognition.onstart = function () {
+    counter.text("voice recognition is on")
+}
+
+recognition.onspeechend = function () {
+    counter.text("no activity")
+
+}
+recognition.onerror = function () {
+    counter.text("try again")
+}
+
+recognition.onresult = function (event) {
+    let current = event.resultIndex;
+    let transcript = event.results[current][0].transcript
+    content += transcript
+    textbox.val(content)
+}
+
+$(".btn").click(function (event) {
+    if (content.length) {
+        content+=''
+    }
+    recognition.start()
+
+})
+
+textbox.on('input', function () {
+    content=$(this).val
+})
