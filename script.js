287ce603fea004d6edaedfd1850ae60fb2f0f777
rajatya713 <rajatya713@gmail.com>
2023-10-07 18:28:07 +0530

    Enhance ui

--- script.js
+++ script.js
@@ -1,41 +1,59 @@
 
-window.SpeechRecognition = window.SpeechRecognition
-    || window.webkitSpeechRecognition;
+if ("webkitSpeechRecognition" in window) {
+    // Initialize webkitSpeechRecognition
+    let speechRecognition = new webkitSpeechRecognition();
 
-let recognition = new SpeechRecognition()
-let textbox = $("#text_box")
-let content = ""
-recognition.continuous = true
-let counter = $("#counter")
+    // String for the Final Transcript
+    let final_transcript = "";
+
+    // Set the properties for the Speech Recognition object
+    speechRecognition.continuous = true;
+    speechRecognition.interimResults = true;
 
 
-recognition.onstart = function () {
-    counter.text("voice recognition is on")
+    // Callback Function for the onStart Event
+    speechRecognition.onstart = () => {
+        // Show the Status Element
+        document.querySelector("#counter").innerHTML="listening...";
+    };
+    speechRecognition.onerror = () => {
+        // Hide the Status Element
+        document.querySelector("#counter").innerHTML = "Error!! while listening";
+    };
+    speechRecognition.onend = () => {
+        // Hide the Status Element
+        document.querySelector("#counter").innerHTML = "";
+    };
+
+    speechRecognition.onresult = (event) => {
+        // Create the interim transcript string locally because we don't want it to persist like final transcript
+        let interim_transcript = "";
+
+        // Loop through the results from the speech recognition object.
+        for (let i = event.resultIndex; i < event.results.length; ++i) {
+            // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+            }
+        }
+
+        // Set the Final transcript and Interim transcript.
+        document.querySelector("#final").innerHTML = final_transcript;
+        document.querySelector("#interim").innerHTML = interim_transcript;
+    };
+
+    // Set the onClick property of the start button
+    document.querySelector("#start").onclick = () => {
+        // Start the Speech Recognition
+        speechRecognition.start();
+    };
+    // Set the onClick property of the stop button
+    document.querySelector("#stop").onclick = () => {
+        // Stop the Speech Recognition
+        speechRecognition.stop();
+    };
+} else {
+    console.log("Speech Recognition Not Available");
 }
-
-recognition.onspeechend = function () {
-    counter.text("no activity")
-
-}
-recognition.onerror = function () {
-    counter.text("try again")
-}
-
-recognition.onresult = function (event) {
-    let current = event.resultIndex;
-    let transcript = event.results[current][0].transcript
-    content += transcript
-    textbox.val(content)
-}
-
-$(".btn").click(function (event) {
-    if (content.length) {
-        content+=''
-    }
-    recognition.start()
-
-})
-
-textbox.on('input', function () {
-    content=$(this).val
-})
